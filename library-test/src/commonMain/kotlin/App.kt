bfc6d20598a0fd76d480da2681eed70734874580
Dawid Jenczewski <djenczewski@simplito.com>
2025-02-25 16:32:23 +0100

    chore: Update test app file App.kt to testing ThreadApi

--- library-test/src/commonMain/kotlin/App.kt
+++ library-test/src/commonMain/kotlin/App.kt
@@ -6,8 +6,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.simplito.java.privmx_endpoint.model.UserWithPubKey
 import com.simplito.java.privmx_endpoint.model.exceptions.PrivmxException
 import com.simplito.java.privmx_endpoint.modules.core.Connection
+import com.simplito.java.privmx_endpoint.modules.thread.ThreadApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -24,17 +26,37 @@ fun App() {
                 withContext(Dispatchers.Default) {
                     launch {
                         try {
+                            val privKey = "L41BJTUy5tKbUzowatSwgHQqycFNoF2WUokdjotRDNwdsGMvXNH8"
                             Connection.connect(
-                                "L41BJTUy5tKbUzowatSwgHQqycFNoF2WUokdjotRDNwdsGMvXNH8",
+                                privKey,
                                 "f42b70a7-4e8c-40fa-a05f-f772aced2a1c",
                                 "http://192.168.24.187:9111",
                             ).use {
-                                it.listContexts(0, 2, sortOrder = "desc").let { pagingList ->
+                                val context = it.listContexts(0, 2, sortOrder = "desc").let { pagingList ->
                                     println(pagingList.totalAvailable)
                                     pagingList.readItems?.forEach { context ->
                                         println("My context: ${context}")
                                     }
+                                    pagingList.readItems?.first()
                                 }
+                                context?.let { context ->
+                                    ThreadApi(it).use { threadApi->
+                                        println("threadApi")
+                                        val threadId="67bdbc0f752a0c0fe5a87a65"
+//                                        threadApi.deleteThread(threadId)
+
+//                                        val thread = threadApi.getThread(threadId)?.also {
+//                                            println(it.threadId + ", " + it.publicMeta?.decodeToString())
+//                                        }
+                                        threadApi.listThreads(context.contextId,0,10,"desc")?.run {
+                                            println(totalAvailable)
+                                            readItems?.forEach { item ->
+                                                println(item?.threadId)
+                                            }
+                                        }
+                                    }
+                                }
+
                             }
                         }catch (e: PrivmxException){
                             println("error ${e.full}")
