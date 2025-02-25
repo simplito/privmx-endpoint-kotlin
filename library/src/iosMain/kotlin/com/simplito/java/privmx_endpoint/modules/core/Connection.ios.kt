16b4f18e45d8e5e46dedbd1fc12ba40578f53d59
Dawid Jenczewski <djenczewski@simplito.com>
2025-02-25 16:30:54 +0100

    Move utils functions from Connection.ios.kt to PsonUtils.kt

--- library/src/iosMain/kotlin/com/simplito/java/privmx_endpoint/modules/core/Connection.ios.kt
+++ library/src/iosMain/kotlin/com/simplito/java/privmx_endpoint/modules/core/Connection.ios.kt
@@ -2,10 +2,10 @@ package com.simplito.java.privmx_endpoint.modules.core
 
 import com.simplito.java.privmx_endpoint.model.Context
 import com.simplito.java.privmx_endpoint.model.PagingList
-import com.simplito.java.privmx_endpoint.model.exceptions.NativeException
-import com.simplito.java.privmx_endpoint.model.exceptions.PrivmxException
+import com.simplito.java.privmx_endpoint.utils.PsonValue
+import com.simplito.java.privmx_endpoint.utils.psonMapper
+import com.simplito.java.privmx_endpoint.utils.PsonResponse
 import kotlinx.cinterop.*
-import kotlinx.cinterop.alloc
 import kotlinx.cinterop.allocPointerTo
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.value
@@ -14,6 +14,8 @@ import libprivmxendpoint.*
 @OptIn(ExperimentalForeignApi::class)
 actual class Connection() : AutoCloseable {
     private val nativeConnection = nativeHeap.allocPointerTo<libprivmxendpoint.Connection>()
+
+    internal fun getConnectionPtr() = nativeConnection.value
 
     actual companion object {
         actual fun connect(userPrivKey: String, solutionId: String, host: String): Connection =
@@ -47,6 +49,9 @@ actual class Connection() : AutoCloseable {
         pson_set_object_value(pagingQuery, "skip", pson_new_int64(skip))
         pson_set_object_value(pagingQuery, "limit", pson_new_int64(limit))
         pson_set_object_value(pagingQuery, "sortOrder", pson_new_string(sortOrder))
+        lastId?.let {
+            pson_set_object_value(pagingQuery, "lastId", pson_new_string(lastId))
+        }
         pson_add_array_value(args, pagingQuery)
 
         privmx_endpoint_execConnection(nativeConnection.value, 3, args, result.ptr)
@@ -78,141 +83,6 @@ actual class Connection() : AutoCloseable {
 
     actual override fun close() {
         disconnect()
-        nativeHeap.free(nativeConnection)
-    }
-}
-
-@OptIn(ExperimentalForeignApi::class)
-internal fun psonMapper(psonValue: CPointer<pson_value>): PsonValue<out Any>? = memScoped {
-    when (pson_value_type(psonValue)) {
-        PSON_BOOL -> {
-            val psonBool = alloc<IntVar>()
-            pson_get_bool(psonValue, psonBool.ptr)
-            return PsonValue.PsonBoolean(psonBool.value == 1)
-        }
-
-        PSON_BINARY -> {
-            val psonBinaryData = allocPointerTo<ByteVar>()
-            val psonBinarySize = alloc<ULongVar>()
-            pson_inspect_binary(psonValue, psonBinaryData.ptr, psonBinarySize.ptr)
-            return PsonValue.PsonBinary(
-                psonBinaryData.value?.readBytes(psonBinarySize.value.toInt()) ?: ByteArray(0)
-            )
-        }
-
-        PSON_INT32 -> {
-            val psonInt = alloc<IntVar>()
-            pson_get_int32(psonValue, psonInt.ptr)
-            return PsonValue.PsonInt(psonInt.value)
-        }
-
-        PSON_INT64 -> {
-            val psonLong = alloc<LongVar>()
-            pson_get_int64(psonValue, psonLong.ptr)
-            return PsonValue.PsonLong(psonLong.value)
-        }
-
-        PSON_FLOAT32 -> {
-            val psonFloat = alloc<FloatVar>()
-            pson_get_float32(psonValue, psonFloat.ptr)
-            return PsonValue.PsonFloat(psonFloat.value)
-        }
-
-        PSON_FLOAT64 -> {
-            val psonDouble = alloc<DoubleVar>()
-            pson_get_float64(psonValue, psonDouble.ptr)
-            return PsonValue.PsonDouble(psonDouble.value)
-        }
-
-        PSON_STRING -> {
-            return PsonValue.PsonString(pson_get_cstring(psonValue)?.toKStringFromUtf8() ?: "")
-        }
-
-        PSON_ARRAY -> {
-            val psonBinarySize = alloc<ULongVar>()
-            pson_get_array_size(psonValue, psonBinarySize.ptr)
-            return PsonValue.PsonArray<PsonValue<Any>>(
-                (0uL..<psonBinarySize.value).map { offset ->
-                    psonMapper(pson_get_array_value(psonValue, offset)!!)
-                }.toList() as List<PsonValue<Any>>
-            )
-        }
-
-        PSON_OBJECT -> {
-            val objectMap = mutableMapOf<String, PsonValue<*>?>()
-            val pson_object_iterator = allocPointerTo<pson_object_iterator>()
-            val current_pson_value = allocPointerTo<pson_value>()
-            val current_pson_value_key = allocPointerTo<ByteVar>()
-            pson_open_object_iterator(psonValue, pson_object_iterator.ptr)
-            while (
-                pson_object_iterator_next(
-                    pson_object_iterator.value,
-                    current_pson_value_key.ptr,
-                    current_pson_value.ptr
-                ) != 0
-            ) {
-                current_pson_value_key.value?.toKStringFromUtf8()?.let { key ->
-                    objectMap[key] = psonMapper(current_pson_value.value!!)
-                }
-            }
-            return PsonValue.PsonObject(objectMap as Map<String, PsonValue<Any>?>)
-        }
-    }
-    return null
-}
-
-@OptIn(ExperimentalForeignApi::class)
-internal class PsonResponse(
-    fromValue: PsonValue.PsonObject
-) {
-    val __type = fromValue["__type"] as PsonValue.PsonString
-    val result = fromValue["result"] as PsonValue.PsonObject?
-    val error = fromValue["error"] as PsonValue.PsonObject?
-    val status = fromValue["status"] as PsonValue.PsonBoolean
-
-    @Throws(
-        PrivmxException::class,
-        NativeException::class
-    )
-    internal fun getResultOrThrow(): PsonValue<*>? {
-        if (error == null && status.getValue()) {
-            return result
-        } else if (error != null && !status.getValue()) {
-            throw if (error["description"] != null) {
-                PrivmxException(
-                    "",
-                    (error["description"] as PsonValue.PsonString).getValue(),
-                    (error["scope"] as PsonValue.PsonString).getValue(),
-                    (error["code"] as PsonValue.PsonLong).getValue().toInt()
-                )
-            } else NativeException(
-                (error["message"] as PsonValue.PsonString?)?.getValue() ?: "Unknown exception"
-            )
-        } else {
-            throw NativeException("Unexpected state of response")
-        }
-    }
-}
-
-internal sealed class PsonValue<T> {
-    protected abstract val value: T
-
-    fun getValue(): T = value
-
-    class PsonBoolean internal constructor(override val value: Boolean) : PsonValue<Boolean>()
-    class PsonLong internal constructor(override val value: Long) : PsonValue<Long>()
-    class PsonInt internal constructor(override val value: Int) : PsonValue<Int>()
-    class PsonDouble internal constructor(override val value: Double) : PsonValue<Double>()
-    class PsonFloat internal constructor(override val value: Float) : PsonValue<Float>()
-    class PsonBinary internal constructor(override val value: ByteArray) : PsonValue<ByteArray>()
-    class PsonString internal constructor(override val value: String) : PsonValue<String>()
-    class PsonArray<T : PsonValue<Any>> internal constructor(override val value: List<T>) :
-        PsonValue<List<T>>() {
-        operator fun get(index: Int): T = this.value[index]
-    }
-
-    class PsonObject internal constructor(override val value: Map<String, PsonValue<Any>?>) :
-        PsonValue<Map<String, PsonValue<Any>?>>() {
-        operator fun get(key: String): PsonValue<Any>? = this.value[key]
+        privmx_endpoint_freeConnection(nativeConnection.value)
     }
 }
