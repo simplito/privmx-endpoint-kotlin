cee3bf804bdbdd8aaae861bf30ab2db9f2064588
Dawid Jenczewski <djenczewski@simplito.com>
2025-02-25 16:27:23 +0100

    feat: Add models converted from Java to Kotlin using IDE converter

---
+++ library/src/commonMain/kotlin/com/simplito/java/privmx_endpoint/model/ServerMessageInfo.kt
@@ -0,0 +1,44 @@
+//
+// PrivMX Endpoint Java.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.simplito.java.privmx_endpoint.model
+
+/**
+ * Holds message's information created by server.
+ *
+ * @category thread
+ * @group Thread
+ */
+class ServerMessageInfo
+/**
+ * Creates instance of `ServerMessageInfo`.
+ *
+ * @param threadId   ID of the message's Thread.
+ * @param messageId  ID of the message.
+ * @param createDate Message's creation timestamp.
+ * @param author     ID of the user who created the message.
+ */(
+    /**
+     * ID of the message's Thread.
+     */
+    var threadId: String?,
+    /**
+     * ID of the message.
+     */
+    var messageId: String?,
+    /**
+     * Message's creation timestamp.
+     */
+    var createDate: Long?,
+    /**
+     * ID of the user who created the message.
+     */
+    var author: String?
+)
---
+++ library/src/commonMain/kotlin/com/simplito/java/privmx_endpoint/model/UserWithPubKey.kt
@@ -0,0 +1,47 @@
+//
+// PrivMX Endpoint Java.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.simplito.java.privmx_endpoint.model
+
+/**
+ * Contains ID of user and the corresponding public key.
+ *
+ * @category core
+ * @group Core
+ */
+class UserWithPubKey {
+    /**
+     * ID of the user.
+     */
+    var userId: String? = null
+
+    /**
+     * User's public key.
+     */
+    var pubKey: String? = null
+
+    /**
+     * Creates instance of `UserWithPubKey`.
+     */
+    constructor()
+
+    /**
+     * Creates instance of `UserWithPubKey`.
+     * @param userId ID of the user.
+     * @param pubKey User's public key.
+     */
+    constructor(
+        userId: String?,
+        pubKey: String?
+    ) {
+        this.userId = userId
+        this.pubKey = pubKey
+    }
+}
