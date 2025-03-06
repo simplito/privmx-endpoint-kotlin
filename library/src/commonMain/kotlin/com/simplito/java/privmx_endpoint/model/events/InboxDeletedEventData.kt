cbfe3a3b2259d9c99435ffebcb654aaeb873de81
Dawid Jenczewski <djenczewski@simplito.com>
2025-03-06 14:35:54 +0100

    feat: Implement Event models and EventQueue class

---
+++ library/src/commonMain/kotlin/com/simplito/java/privmx_endpoint/model/events/InboxDeletedEventData.kt
@@ -0,0 +1,28 @@
+//
+// PrivMX Endpoint Java.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.simplito.java.privmx_endpoint.model.events
+
+/**
+ * Holds information about deleted Inbox.
+ *
+ * @category core
+ * @group Events
+ */
+data class InboxDeletedEventData
+/**
+ * Creates instance of `InboxDeletedEventData`.
+ * @param inboxId ID of the deleted Inbox.
+ */(
+    /**
+     * ID of the deleted Inbox.
+     */
+    val inboxId: String?
+)
