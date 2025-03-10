b507c09ee87995cf59d4e0cc48daf4d90025390d
djenczewski <149203207+djenczewski@users.noreply.github.com>
2025-03-10 14:22:56 +0100

    feat: Implement missing Connection methods (#8)
    * feat: Implement missing Connection methods
    
    * refactor: Make setCertsPath method static

--- library/src/jvmMain/kotlin/com/simplito/java/privmx_endpoint/modules/core/Connection.jvm.kt
+++ library/src/jvmMain/kotlin/com/simplito/java/privmx_endpoint/modules/core/Connection.jvm.kt
@@ -3,7 +3,7 @@ package com.simplito.java.privmx_endpoint.modules.core
 import com.simplito.java.privmx_endpoint.model.Context
 import com.simplito.java.privmx_endpoint.model.PagingList
 
-actual class Connection(private val api: Long?,private val connectionId: Long?) : AutoCloseable {
+actual class Connection private constructor(private val api: Long?,private val connectionId: Long?) : AutoCloseable {
     actual companion object{
         init {
             System.loadLibrary("privmx-endpoint-java")
@@ -13,8 +13,17 @@ actual class Connection(private val api: Long?,private val connectionId: Long?) 
         actual external fun connect(
             userPrivKey: String,
             solutionId: String,
-            host: String,
+            bridgeUrl: String,
         ): Connection
+
+        @JvmStatic
+        actual external fun connectPublic(
+            solutionId: String,
+            bridgeUrl: String,
+        ): Connection
+
+        @JvmStatic
+        actual external fun setCertsPath(certsPath: String?)
     }
 
     private external fun  deinit();
@@ -31,5 +40,7 @@ actual class Connection(private val api: Long?,private val connectionId: Long?) 
     ): PagingList<Context>
 
     actual external fun disconnect()
+
+    actual fun getConnectionId() = this.connectionId
 }
 
