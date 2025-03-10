6d13755bcbcdecfc4c410b5a914895643739a523
Dawid Jenczewski <djenczewski@simplito.com>
2025-03-10 16:30:33 +0100

    feat: Implement SortOrder object

---
+++ privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/java/privmx_endpoint_extra/model/SortOrder.kt
@@ -0,0 +1,29 @@
+//
+// PrivMX Endpoint Java Extra.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.simplito.java.privmx_endpoint_extra.model
+
+
+/**
+ * Constant values for sort order to use in PrivmxEndpoint lists methods.
+ *
+ * @category core
+ */
+object SortOrder {
+    /**
+     * Value for ascending sort order.
+     */
+    const val ASC: String = "asc"
+
+    /**
+     * Value for descending sort order.
+     */
+    const val DESC: String = "desc"
+}
