72438df35c8fd30b7176db141975a0599e5b6bfc
Doominika <101605588+Doominika@users.noreply.github.com>
2025-03-25 08:29:04 +0100

    feat: implement policy builders and policy values (#20)
    * feat: policy builders implementation
    
    * feat: removed unnecessary lines
    
    * feat: implement interfaces which contain scoped methods for setting policies
    
    * feat: convert classes and remove companion objects
    
    * feat: clear code by adding helping function
    
    * feat: change arguments names and replace mutable variables with immutable

---
+++ privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/java/privmx_endpoint_extra/policies/PolicyValue.kt
@@ -0,0 +1,20 @@
+//
+// PrivMX Endpoint Java Extra.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.simplito.java.privmx_endpoint_extra.policies
+
+/**
+ * Base class for all policies values.
+ */
+
+interface PolicyValue
+{
+    val value: String
+}
---
+++ privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/java/privmx_endpoint_extra/policies/SpecialPolicyValue.kt
@@ -0,0 +1,39 @@
+//
+// PrivMX Endpoint Java Extra.
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.simplito.java.privmx_endpoint_extra.policies
+
+/**
+ * Contains special policies values.
+ * Creates instance of [SpecialPolicyValue].
+ *
+ * @param value raw policy value
+ */
+enum class SpecialPolicyValue(override val value: String) : PolicyValue {
+    /**
+     * Uses the default value provided by the Bridge.
+     */
+    DEFAULT("default"),
+
+    /**
+     * Uses the inherited value.
+     */
+    INHERIT("inherit"),
+
+    /**
+     * Allows to perform an action.
+     */
+    YES("yes"),
+
+    /**
+     * Denies to perform an action.
+     */
+    NO("no")
+}
