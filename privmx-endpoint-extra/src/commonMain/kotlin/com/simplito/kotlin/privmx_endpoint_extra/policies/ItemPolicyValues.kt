14029042c26c158df29a1afb137866b5dce15ad4
Dawid Jenczewski <djenczewski@simplito.com>
2025-03-28 16:12:25 +0100

    refactor: move policies to com.simplito.kotlin package

--- privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_extra/policies/ItemPolicyValues.kt
+++ privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_extra/policies/ItemPolicyValues.kt
@@ -8,7 +8,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-package com.simplito.java.privmx_endpoint_extra.policies
+package com.simplito.kotlin.privmx_endpoint_extra.policies
 
 /**
  * Provides a set of predefined values for configuring item policies within a Container.
