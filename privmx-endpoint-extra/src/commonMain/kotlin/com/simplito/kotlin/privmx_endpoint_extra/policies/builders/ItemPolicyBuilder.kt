c07399acdffd0c843dfe7d9a7fbb787d7fe13259
Dawid Jenczewski <djenczewski@simplito.com>
2025-04-01 10:57:52 +0200

    chore: update license in source files

--- privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_extra/policies/builders/ItemPolicyBuilder.kt
+++ privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_extra/policies/builders/ItemPolicyBuilder.kt
@@ -1,3 +1,14 @@
+//
+// PrivMX Endpoint Kotlin Extra.
+// Copyright © 2025 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package com.simplito.kotlin.privmx_endpoint_extra.policies.builders
 
 import com.simplito.kotlin.privmx_endpoint.model.ItemPolicy
--- privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/utils/PsonUtils.kt
+++ privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/utils/PsonUtils.kt
@@ -1,3 +1,14 @@
+//
+// PrivMX Endpoint Kotlin.
+// Copyright © 2025 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 @file:Suppress("MISSING_DEPENDENCY_CLASS_IN_EXPRESSION_TYPE")
 
 package com.simplito.kotlin.privmx_endpoint.utils
