12be7f6834d549903b730ec54665a11cc7b25003
Dawid Jenczewski <djenczewski@simplito.com>
2025-04-14 11:14:53 +0200

    refactor: convert BackendRequester to object and make return type not nullable

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/core/BackendRequester.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/core/BackendRequester.kt
@@ -1,27 +1,25 @@
 package com.simplito.kotlin.privmx_endpoint.modules.core
 
-expect class BackendRequester {
-    companion object {
-        fun backendRequest(
-            serverUrl: String,
-            accessToken: String,
-            method: String,
-            paramsAsJson: String
-        ) : String?
+expect object BackendRequester {
+    fun backendRequest(
+        serverUrl: String,
+        accessToken: String,
+        method: String,
+        paramsAsJson: String
+    ): String
 
-        fun backendRequest(
-            serverUrl: String,
-            method: String,
-            paramsAsJson: String
-        ) : String?
+    fun backendRequest(
+        serverUrl: String,
+        method: String,
+        paramsAsJson: String
+    ): String
 
-        fun backendRequest(
-            serverUrl: String,
-            apiKeyId: String,
-            apiKeySecret: String,
-            mode: Long,
-            method: String,
-            paramsAsJson: String
-        ) : String?
-    }
+    fun backendRequest(
+        serverUrl: String,
+        apiKeyId: String,
+        apiKeySecret: String,
+        mode: Long,
+        method: String,
+        paramsAsJson: String
+    ): String
 }
