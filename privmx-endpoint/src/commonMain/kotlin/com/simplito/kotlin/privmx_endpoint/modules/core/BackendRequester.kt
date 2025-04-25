f434c7bca2b597602d4b23e2273094f78bf97f1b
Dawid Jenczewski <djenczewski@simplito.com>
2025-04-25 12:55:10 +0200

    docs: update BackendRequester.kt docs

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/core/BackendRequester.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/core/BackendRequester.kt
@@ -1,18 +1,28 @@
+//
+// PrivMX Endpoint Kotlin.
+// Copyright © 2025 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package com.simplito.kotlin.privmx_endpoint.modules.core
 
 import com.simplito.kotlin.privmx_endpoint.model.exceptions.NativeException
 import com.simplito.kotlin.privmx_endpoint.model.exceptions.PrivmxException
 
 /**
- * BackendRequester provides functions to call PrivMX Bridge API.
- * (See PrivMX Bridge API for more details)
+ * 'BackendRequester' provides functions to call PrivMX Bridge API.
  */
 expect object BackendRequester {
     /**
      * Sends a request to PrivMX Bridge API using access token for authorization.
      *
      * @param serverUrl PrivMX Bridge server URL
-     * @param accessToken token for authorization
+     * @param accessToken token for authorization (see PrivMX Bridge API for more details)
      * @param method API method to call
      * @param paramsAsJson API method's parameters in JSON format
      *
@@ -47,8 +57,8 @@ expect object BackendRequester {
      * Sends a request to PrivMX Bridge API using pair of API KEY ID and API KEY SECRET for authorization.
      *
      * @param serverUrl PrivMX Bridge server URL
-     * @param apiKeyId API KEY ID
-     * @param apiKeySecret API KEY SECRET
+     * @param apiKeyId API KEY ID (see PrivMX Bridge API for more details)
+     * @param apiKeySecret API KEY SECRET (see PrivMX Bridge API for more details)
      * @param mode allows you to set whether the request should be signed (mode = 1) or plain (mode = 0)
      * @param method API method to call
      * @param paramsAsJson API method's parameters in JSON format
