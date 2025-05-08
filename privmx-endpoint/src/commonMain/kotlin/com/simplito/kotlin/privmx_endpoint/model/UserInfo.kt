4dc0784dc799a4e002c656c91bd085d0148f8f5e
Doominika <dsluzynska@simplito.com>
2025-05-08 13:49:20 +0200

    feat: add UserInfo class

---
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/UserInfo.kt
@@ -0,0 +1,13 @@
+package com.simplito.kotlin.privmx_endpoint.model
+
+/**
+ * Contains Information about user
+ *
+ * @property user       User publicKey and userId
+ * @property isActive    is user connected to bridge
+ */
+class UserInfo
+    (
+    var user: UserWithPubKey,
+    var isActive: Boolean
+)
