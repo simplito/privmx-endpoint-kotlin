997643da6502e98bf69761ba3a7701db74aa19fe
Doominika <dsluzynska@simplito.com>
2025-05-09 12:23:32 +0200

    feat: add Utils in jvm

---
+++ privmx-endpoint/src/jvmMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/utils/Hex.jvm.kt
@@ -0,0 +1,27 @@
+package com.simplito.java.privmx_endpoint.model.utils
+
+actual object Hex {
+    /**
+     * Encodes buffer to a string in Hex format.
+     *
+     * @param data buffer to encode
+     * @return string in Hex format
+     */
+    actual external fun encode(data: ByteArray): String
+
+    /**
+     * Decodes string in Hex to buffer.
+     *
+     * @param hexData string to decode
+     * @return buffer with decoded data
+     */
+    actual external  fun decode(hexData: String): ByteArray
+
+    /**
+     * Checks if given string is in Hex format.
+     *
+     * @param data string to check
+     * @return data check result
+     */
+    actual external fun `is`(data: String): Boolean
+}
