d39d4336b0722c6f5ee6d61df4ec43f7ffaaad1a
Doominika <dsluzynska@simplito.com>
2025-05-22 11:32:03 +0200

    refactor: add new schemaVersion parameter to Inbox

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/Inbox.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/Inbox.kt
@@ -27,6 +27,7 @@ package com.simplito.kotlin.privmx_endpoint.model
  * @property filesConfig          Inbox files configuration
  * @property policy               Inbox policies
  * @property statusCode           Status code of retrieval and decryption of the Inbox
+ * @property schemaVersion        Version of the Inbox data structure and how it is encoded/encrypted.
  */
 data class Inbox(
     val inboxId: String,
@@ -42,8 +43,61 @@ data class Inbox(
     val privateMeta: ByteArray,
     val filesConfig: FilesConfig?,
     val policy: ContainerPolicyWithoutItem,
-    val statusCode: Long?
+    val statusCode: Long?,
+    val schemaVersion: Long?
 ) {
+    /**
+     * Holds all available information about an Inbox.
+     *
+     * @property inboxId              ID of the Inbox
+     * @property contextId            ID of the Context
+     * @property createDate           Inbox creation timestamp
+     * @property creator              ID of the user who created the Inbox
+     * @property lastModificationDate Inbox last modification timestamp
+     * @property lastModifier         ID of the user who last modified the Inbox
+     * @property users                List of users (their IDs) with access to the Inbox
+     * @property managers             List of users (their IDs) with management rights
+     * @property version              Version number (changes on updates)
+     * @property publicMeta           Inbox public metadata
+     * @property privateMeta          Inbox private metadata
+     * @property filesConfig          Inbox files configuration
+     * @property policy               Inbox policies
+     * @property statusCode           Status code of retrieval and decryption of the Inbox
+     */
+    @Deprecated("Use primary constructor with new parameter.")
+    constructor(
+        inboxId: String,
+        contextId: String,
+        createDate: Long?,
+        creator: String,
+        lastModificationDate: Long?,
+        lastModifier: String,
+        users: List<String>,
+        managers: List<String>,
+        version: Long?,
+        publicMeta: ByteArray,
+        privateMeta: ByteArray,
+        filesConfig: FilesConfig?,
+        policy: ContainerPolicyWithoutItem,
+        statusCode: Long?,
+    ) : this(
+        inboxId,
+        contextId,
+        createDate,
+        creator,
+        lastModificationDate,
+        lastModifier,
+        users,
+        managers,
+        version,
+        publicMeta,
+        privateMeta,
+        filesConfig,
+        policy,
+        statusCode,
+        null
+    )
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || this::class != other::class) return false
@@ -64,6 +118,7 @@ data class Inbox(
         if (!privateMeta.contentEquals(other.privateMeta)) return false
         if (filesConfig != other.filesConfig) return false
         if (policy != other.policy) return false
+        if (schemaVersion != other.schemaVersion) return false
 
         return true
     }
@@ -83,6 +138,8 @@ data class Inbox(
         result = 31 * result + privateMeta.contentHashCode()
         result = 31 * result + (filesConfig?.hashCode() ?: 0)
         result = 31 * result + policy.hashCode()
+        result = 31 * result + (schemaVersion?.hashCode() ?: 0)
+
         return result
     }
 }
