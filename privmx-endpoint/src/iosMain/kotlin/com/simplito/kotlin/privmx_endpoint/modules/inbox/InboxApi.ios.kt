f7794eda5d37beed0af4853a7276f147f438bde3
Doominika <dsluzynska@simplito.com>
2025-05-23 15:29:40 +0200

    docs: add userPrivKey description to prepareEntry method

--- privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/inbox/InboxApi.ios.kt
+++ privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/inbox/InboxApi.ios.kt
@@ -341,6 +341,9 @@ actual constructor(
      * @param inboxId          ID of the Inbox to which the request applies
      * @param data             entry data to send
      * @param inboxFileHandles optional list of file handles that will be sent with the request
+     * @param userPrivKey sender can optionally provide a private key, which will be used:
+     * 1) to sign the sent data,
+     * 2) to derivation of the public key, which will then be transferred along with the sent data and can be used in the future for further secure communication with the sender
      * @return Inbox handle
      * @throws PrivmxException       thrown when method encounters an exception
      * @throws NativeException       thrown when method encounters an unknown exception
