dd70349071e491a3a60574d00c593abd4a579b57
Doominika <101605588+Doominika@users.noreply.github.com>
2025-06-12 16:06:22 +0200

    feat: Implement features from 2.3 for iOS (#63)
    * refactor: add new queryAsJson argument to listContexts method
    
    * refactor: add new queryAsJson argument to listContexts method
    
    * feat: remove jvm changes from ios branch
    
    * feat: add getContextUsers method to Connection
    
    * feat: add verificationOptions to connect and connectPublic methods
    
    * feat:: implement ExtKey
    
    * feat:: add new queryAsJson argument to listInboxes and listEntries methods
    
    * feat:: add new queryAsJson argument to listStores and listFiles methods
    
    * feat:: add new queryAsJson argument to listThreads and listMessages methods
    
    * feat: implement EventApi
    
    * feat: implement new methods in CryptoApi
    
    * refactor: change BIP39_t name to BIP39
    
    * refactor: change BIP39_t name to BIP39
    
    * refactor: add new schemaVersion parameter to Store
    
    * refactor: add new schemaVersion parameter to File
    
    * refactor: add new schemaVersion parameter to Thread
    
    * refactor: add new schemaVersion parameter to Message
    
    * refactor: add new schemaVersion parameter to Inbox
    
    * refactor: add new schemaVersion parameter to InboxEntry
    
    * docs: add description of verificationOptions to connect and connectPublic methods and add annotations about exceptions
    
    * feat: implement Utils classes - base32, base64, hex, utils
    
    * docs: add userPrivKey description to prepareEntry method
    
    * feat(privmx-endpoint/iOS): Implement setUserVerifier method (#62)
    
    * feat: implement setUserVerifier
    
    * fix(privmx-endpoint/ios): fix native C implementation of verifier function
    
    ---------
    
    Co-authored-by: Dawid Jenczewski <djenczewski@simplito.com>
    
    * Revert common module changes
    
    * Revert jvm module changes
    
    * fix(privmx-endpoint/ios): ExtKey implementation
    
    * refactor(privmx-endpoint/ios): getContextUsers method and throws annotations in Connection.ios.kt
    
    * refactor(privmx-endpoint/ios): CryptoApi.ios.kt 2.3 features implementation
    
    * fix(privmx-endpoint/ios): pson parsers for new fields and models from 2.3.0
    
    * docs(privmx-endpoint/ios): add description to queryAsJson param in InboxApi.ios.kt
    
    * chore(privmx-endpoint/ios): reformat code in InboxApi.ios.kt
    
    * refactor(privmx-endpoint/ios): implementation and throws annotations in EventApi.ios.kt
    
    * refactor(privmx-endpoint/ios): Utils classes implementation
    
    * refactor(privmx-endpoint/ios): move Utils to core module
    
    * fix(privmx-endpoint/ios): fix ExtKey initialization and Throws annotation
    
    ---------
    
    Co-authored-by: Dawid Jenczewski <djenczewski@simplito.com>

---
+++ privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/event/EventApi.ios.kt
@@ -0,0 +1,100 @@
+package com.simplito.kotlin.privmx_endpoint.modules.event
+
+import cnames.structs.pson_value
+import com.simplito.kotlin.privmx_endpoint.model.UserWithPubKey
+import com.simplito.kotlin.privmx_endpoint.model.exceptions.NativeException
+import com.simplito.kotlin.privmx_endpoint.model.exceptions.PrivmxException
+import com.simplito.kotlin.privmx_endpoint.modules.core.Connection
+import com.simplito.kotlin.privmx_endpoint.utils.asResponse
+import com.simplito.kotlin.privmx_endpoint.utils.makeArgs
+import com.simplito.kotlin.privmx_endpoint.utils.pson
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.allocPointerTo
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.nativeHeap
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.value
+import libprivmxendpoint.privmx_endpoint_execEventApi
+import libprivmxendpoint.privmx_endpoint_freeEventApi
+import libprivmxendpoint.privmx_endpoint_newEventApi
+import libprivmxendpoint.pson_free_result
+import libprivmxendpoint.pson_free_value
+import libprivmxendpoint.pson_new_array
+
+@OptIn(ExperimentalForeignApi::class)
+actual class EventApi
+@Throws(IllegalStateException::class)
+actual constructor(connection: Connection) : AutoCloseable {
+    private val _nativeEventApi = nativeHeap.allocPointerTo<cnames.structs.EventApi>()
+    private val nativeEventApi
+        get() = _nativeEventApi.value?.let { _nativeEventApi }
+            ?: throw IllegalStateException("EventApi has been closed.")
+
+    internal fun getEventPtr() = nativeEventApi.value
+
+    init {
+        privmx_endpoint_newEventApi(connection.getConnectionPtr(), _nativeEventApi.ptr)
+        memScoped {
+            val args = pson_new_array()
+            val result = allocPointerTo<pson_value>()
+            try {
+                privmx_endpoint_execEventApi(nativeEventApi.value, 0, args, result.ptr)
+                result.value!!.asResponse?.getResultOrThrow()
+            } finally {
+                pson_free_value(args)
+                pson_free_result(result.value)
+            }
+        }
+    }
+
+    @Throws(PrivmxException::class, NativeException::class, IllegalStateException::class)
+    actual fun emitEvent(
+        contextId: String, users: List<UserWithPubKey>, channelName: String, eventData: ByteArray
+    ): Unit = memScoped {
+        val result = allocPointerTo<pson_value>()
+        val args = makeArgs(
+            contextId.pson, users.map { it.pson }.pson, channelName.pson, eventData.pson
+        )
+        try {
+            privmx_endpoint_execEventApi(nativeEventApi.value, 1, args, result.ptr)
+            result.value?.asResponse?.getResultOrThrow()
+        } finally {
+            pson_free_value(args)
+            pson_free_result(result.value)
+        }
+    }
+
+    @Throws(PrivmxException::class, NativeException::class, IllegalStateException::class)
+    actual fun subscribeForCustomEvents(contextId: String, channelName: String): Unit = memScoped {
+        val result = allocPointerTo<pson_value>()
+        val args = makeArgs(
+            contextId.pson, channelName.pson
+        )
+        try {
+            privmx_endpoint_execEventApi(nativeEventApi.value, 2, args, result.ptr)
+            result.value?.asResponse?.getResultOrThrow()
+        } finally {
+            pson_free_value(args)
+            pson_free_result(result.value)
+        }
+    }
+
+    @Throws(PrivmxException::class, NativeException::class, IllegalStateException::class)
+    actual fun unsubscribeFromCustomEvents(contextId: String, channelName: String): Unit =
+        memScoped {
+            val result = allocPointerTo<pson_value>()
+            val args = makeArgs(contextId.pson, channelName.pson)
+            try {
+                privmx_endpoint_execEventApi(nativeEventApi.value, 3, args, result.ptr)
+                result.value?.asResponse?.getResultOrThrow()
+            } finally {
+                pson_free_value(args)
+                pson_free_result(result.value)
+            }
+        }
+
+    actual override fun close() {
+        privmx_endpoint_freeEventApi(nativeEventApi.value)
+        _nativeEventApi.value = null
+    }
+}
--- privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/inbox/InboxApi.ios.kt
+++ privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/inbox/InboxApi.ios.kt
@@ -252,6 +252,7 @@ actual constructor(
      * @param limit     limit of elements to return for query
      * @param sortOrder order of elements in result ("asc" for ascending, "desc" for descending)
      * @param lastId    ID of the element from which query results should start
+     * @param queryAsJson stringified JSON object with a custom field to filter result
      * @return list of Inboxes
      * @throws PrivmxException       thrown when method encounters an exception
      * @throws NativeException       thrown when method encounters an unknown exception
@@ -261,7 +262,12 @@ actual constructor(
         PrivmxException::class, NativeException::class, IllegalStateException::class
     )
     actual fun listInboxes(
-        contextId: String, skip: Long, limit: Long, sortOrder: String, lastId: String?
+        contextId: String,
+        skip: Long,
+        limit: Long,
+        sortOrder: String,
+        lastId: String?,
+        queryAsJson: String?
     ): PagingList<Inbox> = memScoped {
         val pson_result = allocPointerTo<pson_value>()
         val args = makeArgs(
@@ -269,7 +275,9 @@ actual constructor(
                 "skip" to skip.pson,
                 "limit" to limit.pson,
                 "sortOrder" to sortOrder.pson,
-                lastId?.let { "lastId" to lastId.pson }).pson
+                lastId?.let { "lastId" to lastId.pson },
+                queryAsJson?.let { "queryAsJson" to queryAsJson.pson }
+            ).pson
         )
         try {
             privmx_endpoint_execInboxApi(nativeInboxApi.value, 4, args, pson_result.ptr)
@@ -339,6 +347,9 @@ actual constructor(
      * @param inboxId          ID of the Inbox to which the request applies
      * @param data             entry data to send
      * @param inboxFileHandles optional list of file handles that will be sent with the request
+     * @param userPrivKey sender can optionally provide a private key, which will be used:
+     * 1) to sign the sent data,
+     * 2) to derivation of the public key, which will then be transferred along with the sent data and can be used in the future for further secure communication with the sender
      * @return Inbox handle
      * @throws PrivmxException       thrown when method encounters an exception
      * @throws NativeException       thrown when method encounters an unknown exception
@@ -424,6 +435,7 @@ actual constructor(
      * @param limit     limit of elements to return for query
      * @param sortOrder order of elements in result ("asc" for ascending, "desc" for descending)
      * @param lastId    ID of the element from which query results should start
+     * @param queryAsJson stringified JSON object with a custom field to filter result
      * @return list of entries
      * @throws PrivmxException       thrown when method encounters an exception
      * @throws NativeException       thrown when method encounters an unknown exception
@@ -433,7 +445,12 @@ actual constructor(
         PrivmxException::class, NativeException::class, IllegalStateException::class
     )
     actual fun listEntries(
-        inboxId: String, skip: Long, limit: Long, sortOrder: String, lastId: String?
+        inboxId: String,
+        skip: Long,
+        limit: Long,
+        sortOrder: String,
+        lastId: String?,
+        queryAsJson: String?
     ): PagingList<InboxEntry> = memScoped {
         val pson_result = allocPointerTo<pson_value>()
         val args = makeArgs(
@@ -441,7 +458,9 @@ actual constructor(
                 "skip" to skip.pson,
                 "limit" to limit.pson,
                 "sortOrder" to sortOrder.pson,
-                lastId?.let { "lastId" to lastId.pson }).pson
+                lastId?.let { "lastId" to lastId.pson },
+                queryAsJson?.let { "queryAsJson" to queryAsJson.pson }
+            ).pson
         )
         try {
             privmx_endpoint_execInboxApi(nativeInboxApi.value, 10, args, pson_result.ptr)
