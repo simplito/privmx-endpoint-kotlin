1e8d7c08252c5cf429d41f0b0929e067d3217246
Dawid Jenczewski <djenczewski@simplito.com>
2025-06-23 14:54:00 +0200

    chore(privmx-endpoint): Update license in Kotlin files created in 2.3

--- privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/core/utils/Base32.ios.kt
+++ privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/core/utils/Base32.ios.kt
@@ -1,3 +1,14 @@
+//
+// PrivMX Endpoint Kotlin.
+// Copyright © 2025 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package com.simplito.kotlin.privmx_endpoint.modules.core.utils
 
 import cnames.structs.pson_value
