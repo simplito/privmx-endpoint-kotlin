aaf74b0356c1e4024fd901de206f54228d86581b
Dawid Jenczewski <djenczewski@simplito.com>
2025-07-07 12:54:46 +0200

    refactor: fix package for kvdb event data classes

--- privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_extra/events/EventType.kt
+++ privmx-endpoint-extra/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_extra/events/EventType.kt
@@ -10,9 +10,9 @@
 //
 package com.simplito.kotlin.privmx_endpoint_extra.events
 
-import com.simplito.java.privmx_endpoint.model.events.KvdbDeletedEntryEventData
-import com.simplito.java.privmx_endpoint.model.events.KvdbDeletedEventData
-import com.simplito.java.privmx_endpoint.model.events.KvdbStatsEventData
+import com.simplito.kotlin.privmx_endpoint.model.events.KvdbDeletedEntryEventData
+import com.simplito.kotlin.privmx_endpoint.model.events.KvdbDeletedEventData
+import com.simplito.kotlin.privmx_endpoint.model.events.KvdbStatsEventData
 import com.simplito.kotlin.privmx_endpoint.model.File
 import com.simplito.kotlin.privmx_endpoint.model.Inbox
 import com.simplito.kotlin.privmx_endpoint.model.InboxEntry
