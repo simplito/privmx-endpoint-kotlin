f072ce0aa024dff5e7e29150ff3cc8dfc979a643
Doominika <dsluzynska@simplito.com>
2025-07-21 12:42:46 +0200

    docs: change list to stet in deleteEntries method description

--- privmx-endpoint/src/jvmMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/kvdb/KvdbApi.jvm.kt
+++ privmx-endpoint/src/jvmMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/kvdb/KvdbApi.jvm.kt
@@ -279,7 +279,7 @@ actual class KvdbApi actual constructor(connection: Connection) : AutoCloseable 
     )
 
     /**
-     * Deletes KVDB entries by given KVDB IDs and the list of entry keys.
+     * Deletes KVDB entries by given KVDB IDs and the set of entry keys.
      *
      * @param kvdbId ID of the KVDB database to delete from
      * @param keys   set of the keys of the KVDB entries to delete
