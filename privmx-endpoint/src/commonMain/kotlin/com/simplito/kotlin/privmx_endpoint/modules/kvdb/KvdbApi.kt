dc4e85b55f04af822186fd94458df719d1f48136
Doominika <dsluzynska@simplito.com>
2025-08-07 13:12:36 +0200

    refactor: delete old subscription methods from KvdbApi - common

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/kvdb/KvdbApi.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/kvdb/KvdbApi.kt
@@ -258,48 +258,6 @@ expect class KvdbApi(connection: Connection) : AutoCloseable {
     ): Map<String, Boolean>
 
     /**
-     * Subscribes for the KVDB module main events.
-     *
-     * @throws IllegalStateException thrown when instance is closed.
-     * @throws PrivmxException       thrown when method encounters an exception.
-     * @throws NativeException       thrown when method encounters an unknown exception.
-     */
-    @Throws(PrivmxException::class, NativeException::class, IllegalStateException::class)
-    fun subscribeForKvdbEvents()
-
-    /**
-     * Unsubscribes from the KVDB module main events.
-     *
-     * @throws IllegalStateException thrown when instance is closed.
-     * @throws PrivmxException       thrown when method encounters an exception.
-     * @throws NativeException       thrown when method encounters an unknown exception.
-     */
-    @Throws(PrivmxException::class, NativeException::class, IllegalStateException::class)
-    fun unsubscribeFromKvdbEvents()
-
-    /**
-     * Subscribes for events in given KVDB.
-     *
-     * @param kvdbId ID of the KVDB to subscribe
-     * @throws IllegalStateException thrown when instance is closed.
-     * @throws PrivmxException       thrown when method encounters an exception.
-     * @throws NativeException       thrown when method encounters an unknown exception.
-     */
-    @Throws(PrivmxException::class, NativeException::class, IllegalStateException::class)
-    fun subscribeForEntryEvents(kvdbId: String)
-
-    /**
-     * Unsubscribes from events in given KVDB.
-     *
-     * @param kvdbId ID of the KVDB to unsubscribe
-     * @throws IllegalStateException thrown when instance is closed.
-     * @throws PrivmxException       thrown when method encounters an exception.
-     * @throws NativeException       thrown when method encounters an unknown exception.
-     */
-    @Throws(PrivmxException::class, NativeException::class, IllegalStateException::class)
-    fun unsubscribeFromEntryEvents(kvdbId: String)
-
-    /**
      * Frees memory.
      *
      * @throws Exception when instance is currently closed.
