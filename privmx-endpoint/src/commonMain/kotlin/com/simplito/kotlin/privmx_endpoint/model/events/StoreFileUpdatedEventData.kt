de317accbdc3e6760b8835e9a969937c4c16947e
Doominika <dsluzynska@simplito.com>
2025-08-07 17:41:38 +0200

    feat: add StoreFileUpdatedEventData class

---
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/events/StoreFileUpdatedEventData.kt
@@ -0,0 +1,15 @@
+package com.simplito.kotlin.privmx_endpoint.model.events
+
+import com.simplito.kotlin.privmx_endpoint.model.FileChange
+import com.simplito.kotlin.privmx_endpoint.model.File
+
+/**
+ * Holds information about the file change.
+ *
+ * @property file    File meta.
+ * @property changes List of file changes.
+ */
+class StoreFileUpdatedEventData(
+    file: File?,
+    changes: List<FileChange>
+)
