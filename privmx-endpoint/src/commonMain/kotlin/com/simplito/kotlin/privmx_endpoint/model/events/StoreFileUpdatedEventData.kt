e87d3e12d61eae67958866457f9f5393d08f6892
Doominika <dsluzynska@simplito.com>
2025-08-27 14:28:21 +0200

    docs: update StoreFileUpdatedEventData class description

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/events/StoreFileUpdatedEventData.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/events/StoreFileUpdatedEventData.kt
@@ -1,10 +1,10 @@
 package com.simplito.kotlin.privmx_endpoint.model.events
 
-import com.simplito.kotlin.privmx_endpoint.model.FileChange
 import com.simplito.kotlin.privmx_endpoint.model.File
+import com.simplito.kotlin.privmx_endpoint.model.FileChange
 
 /**
- * Holds information about the file change.
+ * Holds information about file updates.
  *
  * @property file    File meta.
  * @property changes List of file changes.
