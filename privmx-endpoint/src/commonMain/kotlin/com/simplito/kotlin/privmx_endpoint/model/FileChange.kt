0f7c11df25b2c4d9c7454fcf6858aacd19c0a7af
Doominika <dsluzynska@simplito.com>
2025-09-23 13:42:26 +0200

    docs: update FileChange class params descriptions

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/FileChange.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/FileChange.kt
@@ -3,8 +3,8 @@ package com.simplito.kotlin.privmx_endpoint.model
 /**
  * Holds information about the file change.
  *
- * @property pos      Position of file change.
- * @property length   Length of file change.
+ * @property pos      Position of the first changed chunk.
+ * @property length   Length aligned to full chunks.
  * @property  truncate Remove all data.
  */
 class FileChange(
