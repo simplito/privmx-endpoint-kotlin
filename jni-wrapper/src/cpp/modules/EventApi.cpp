2f2fe3853a298348ebb96587813c816260f1184e
Doominika <dsluzynska@simplito.com>
2025-10-07 13:27:18 +0200

    feat: update EventApi file

--- jni-wrapper/src/cpp/modules/EventApi.cpp
+++ jni-wrapper/src/cpp/modules/EventApi.cpp
@@ -105,47 +105,111 @@ Java_com_simplito_kotlin_privmx_1endpoint_modules_event_EventApi_emitEvent(
 }
 
 extern "C"
-JNIEXPORT void JNICALL
-Java_com_simplito_kotlin_privmx_1endpoint_modules_event_EventApi_subscribeForCustomEvents(
+JNIEXPORT jobject JNICALL
+Java_com_simplito_kotlin_privmx_1endpoint_modules_event_EventApi_subscribeFor(
         JNIEnv *env,
         jobject thiz,
-        jstring context_id,
-        jstring channel_name
+        jobject subscription_queries
 ) {
     JniContextUtils ctx(env);
-
-    if (ctx.nullCheck(context_id, "Context ID") ||
-        ctx.nullCheck(channel_name, "Channel name")) {
-        return;
+    if (ctx.nullCheck(subscription_queries, "Subscription queries")) {
+        return nullptr;
     }
 
-    ctx.callVoidEndpointApi([&ctx, &thiz, &context_id, &channel_name]() {
-        getEventApi(ctx, thiz)->subscribeForCustomEvents(
-                ctx.jString2string(context_id),
-                ctx.jString2string(channel_name)
-        );
-    });
+    jobject result = nullptr;
+    ctx.callResultEndpointApi<jobject>(
+            &result,
+            [&ctx, &env, &thiz, &subscription_queries]() {
+                jclass arrayListCls = env->FindClass("java/util/ArrayList");
+                jmethodID initMID = env->GetMethodID(arrayListCls, "<init>", "()V");
+                jmethodID addToListMID = env->GetMethodID(arrayListCls, "add", "(Ljava/lang/Object;)Z");
+
+                auto subscription_queries_arr = ctx.jObject2jArray(subscription_queries);
+                auto subscription_queries_c = std::vector<std::string>();
+
+                int length = ctx->GetArrayLength(subscription_queries_arr);
+                for (int i = 0; i < length; i++) {
+                    jobject arrayElement = ctx->GetObjectArrayElement(subscription_queries_arr, i);
+                    subscription_queries_c.push_back(ctx.jString2string((jstring) arrayElement));
+                }
+
+                jobject arrayList = env->NewObject(arrayListCls, initMID);
+                auto subscription_ids_c = getEventApi(ctx, thiz)->
+                        subscribeFor(subscription_queries_c);
+
+                for (auto &id_str : subscription_ids_c) {
+                    jstring java_id_str = ctx->NewStringUTF(id_str.c_str());
+                    env->CallBooleanMethod(arrayList, addToListMID, java_id_str);
+                }
+                return arrayList;
+            }
+    );
+    if (ctx->ExceptionCheck()) {
+        return nullptr;
+    }
+    return result;
 }
+
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_simplito_kotlin_privmx_1endpoint_modules_event_EventApi_unsubscribeFromCustomEvents(
+Java_com_simplito_kotlin_privmx_1endpoint_modules_event_EventApi_unsubscribeFrom(
         JNIEnv *env,
         jobject thiz,
-        jstring context_id,
-        jstring channel_name
+        jobject subscription_ids
 ) {
     JniContextUtils ctx(env);
-
-    if (ctx.nullCheck(context_id, "Context ID") ||
-        ctx.nullCheck(channel_name, "Channel name")) {
+    if (ctx.nullCheck(subscription_ids, "Subscription IDs")) {
         return;
     }
 
-    ctx.callVoidEndpointApi([&ctx, &thiz, &context_id, &channel_name]() {
-        getEventApi(ctx, thiz)->unsubscribeFromCustomEvents(
-                ctx.jString2string(context_id),
-                ctx.jString2string(channel_name)
-        );
+    ctx.callVoidEndpointApi([&ctx, &env, &thiz, &subscription_ids]() {
+        auto subscription_ids_arr = ctx.jObject2jArray(subscription_ids);
+        auto subscription_ids_c = std::vector<std::string>();
+
+        int length = ctx->GetArrayLength(subscription_ids_arr);
+        for (int i = 0; i < length; i++) {
+            jobject arrayElement = ctx->GetObjectArrayElement(subscription_ids_arr, i);
+        if (ctx.nullCheck(arrayElement, "Subscription ids array elements")) {
+            return;
+        }
+            subscription_ids_c.push_back(ctx.jString2string((jstring) arrayElement));
+        }
+
+        getEventApi(ctx, thiz)->unsubscribeFrom(subscription_ids_c);
     });
+}
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_simplito_kotlin_privmx_1endpoint_modules_event_EventApi_buildSubscriptionQuery(
+        JNIEnv *env,
+        jobject thiz,
+        jstring channel_name,
+        jlong selector_type,
+        jstring selector_id
+) {
+    JniContextUtils ctx(env);
+    if (ctx.nullCheck(channel_name, "ChannelName") ||
+        ctx.nullCheck(selector_id, "SelectorID")) {
+        return nullptr;
+    }
+
+    jstring result = nullptr;
+    ctx.callResultEndpointApi<jstring>(
+            &result,
+            [&ctx, &env, &thiz, &channel_name, &selector_type, &selector_id]() {
+                std::string query_result_c = getEventApi(ctx, thiz)->buildSubscriptionQuery(
+                        ctx.jString2string(channel_name),
+                        static_cast<event::EventSelectorType>(selector_type),
+                        ctx.jString2string(selector_id)
+                );
+                return ctx->NewStringUTF(query_result_c.c_str());
+            }
+    );
+    if (ctx->ExceptionCheck()) {
+        return nullptr;
+    }
+    return result;
 }
