8683f829ffc1521ba9a7132eef138f20a7b05325
Doominika <dsluzynska@simplito.com>
2026-04-24 14:07:04 +0200

    feat: add event selectors for streams

---
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/eventTypes/StreamEventType.kt
@@ -0,0 +1,14 @@
+package com.simplito.java.privmx_endpoint.model.stream.events.eventTypes
+
+import com.simplito.kotlin.privmx_endpoint.model.events.eventTypes.EventType
+
+enum class StreamEventType : EventType {
+    STREAMROOM_CREATE,
+    STREAMROOM_UPDATE,
+    STREAMROOM_DELETE,
+    EMPTY,
+    STREAM_JOIN,
+    STREAM_LEAVE,
+    STREAM_PUBLISH,
+    STREAM_UNPUBLISH
+}
