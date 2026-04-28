702e28dca65934fcaa3ce47f253ab9253b344345
Doominika <dsluzynska@simplito.com>
2026-04-28 14:02:16 +0200

    chore: format code

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/StreamPublishedEventData.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/StreamPublishedEventData.kt
@@ -5,7 +5,6 @@ import com.simplito.kotlin.privmx_endpoint.model.stream.StreamInfo
 
 data class StreamPublishedEventData(
     val streamRoomId: String,
-    val stream:
-    StreamInfo,
+    val stream: StreamInfo,
     val userId: String
 )
