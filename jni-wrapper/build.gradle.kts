7590475a3e355b578cccfe0e175c1423ed865bd0
Dawid Jenczewski <djenczewski@simplito.com>
2026-04-29 12:04:12 +0200

    chore(jni-wrappers): prepare gradle tasks for build android and darwin jni binaries using conan and new CMakeLists.txt

--- jni-wrapper/build.gradle.kts
+++ jni-wrapper/build.gradle.kts
@@ -9,6 +9,11 @@ plugins {
 library {
     linkage.set(listOf(Linkage.SHARED))
 }
+enum class BuildTypes {
+    Release,
+    Debug,
+    MinSizeRel
+}
 
 val localProperties = Properties().apply {
     load(file(rootDir.absolutePath + "/local.properties").inputStream())
@@ -20,14 +25,14 @@ val darwinArchs = listOf("arm64")
 //TODO: Use cpp-library plugin compilation tasks
 val compileAndroid = tasks.create("compileAndroid") {
     group = "privmx native"
+    dependsOn("buildAndroidWithConan")
     val sdkDir = localProperties.getProperty("sdk.dir")
     val ndkVersion = localProperties.getProperty("ndk.version")
     val compileDir = layout.buildDirectory.dir("native/compile").get()
+    val installDir = layout.buildDirectory.dir("native/install").get()
     val androidNdkPath = "$sdkDir/ndk/$ndkVersion"
-    val androidToolChainPath = "$sdkDir/ndk/$ndkVersion/build/cmake/android.toolchain.cmake"
     val os = "Android"
     val APILevel = "24"
-    val installDir = layout.buildDirectory.dir("native/install").get()
     doFirst {
         androidArchs.onEach { ARCH ->
             val platformInstallDir = installDir.file("$os/$privmxEndpointJavaVersion/$ARCH").asFile
@@ -38,30 +43,33 @@ val compileAndroid = tasks.create("compileAndroid") {
             if (!platformInstallDir.exists()) {
                 platformInstallDir.mkdirs()
             }
-
             exec {
-                workingDir = layout.projectDirectory.dir("src/cpp/").asFile
+                workingDir = layout.projectDirectory.asFile
                 commandLine(
                     "sh", "-c",
                     "cmake" +
                             " -B${platformCompileDir.absolutePath}" +
-                            " -DCMAKE_BUILD_TYPE=Release" +
+                            " -DCMAKE_BUILD_TYPE=${buildType.name}" +
                             " -DANDROID_NDK=\"$androidNdkPath\"" +
-                            " -DCMAKE_TOOLCHAIN_FILE=\"$androidToolChainPath\"" +
                             " -DCMAKE_CXX_FLAGS=-std=c++17" +
-                            " -DCMAKE_DESTINATION_OS=$os" +
-                            " -DCMAKE_DESTINATION_ARCHITECTURE=\"$ARCH\"" +
                             " -DANDROID_PLATFORM=\"android-$APILevel\"" +
                             " -DANDROID_ABI=\"$ARCH\"" +
                             " -DJAVA_HOME=\"${Jvm.current().javaHome}\"" +
-                            " -DCMAKE_INSTALL_PREFIX=\"${platformInstallDir.absolutePath}\"",
+                            " -DCMAKE_INSTALL_PREFIX=\"${platformInstallDir.absolutePath}\"" +
+                            " -DCMAKE_TOOLCHAIN_FILE=\"conan_android_toolchain.cmake\""
                 )
+            }
+
+            exec {
+                workingDir = platformCompileDir
+                commandLine("sh", "-c", "cmake --build .")
             }
 
             exec {
                 workingDir = platformCompileDir
                 commandLine("sh", "-c", "make -s -j8")
             }
+
             exec {
                 workingDir = platformCompileDir
                 commandLine("sh", "-c", "make -s install")
@@ -70,11 +78,16 @@ val compileAndroid = tasks.create("compileAndroid") {
     }
 
 }
+
+val buildType = BuildTypes.Debug
+
 //TODO: Use cpp-library plugin compilation tasks
 val compileDarwin = tasks.create("compileDarwin") {
     group = "privmx native"
-    val compileDir = layout.buildDirectory.dir("native/compile").get()
+    dependsOn("buildMacosWithConan")
     val os = "Darwin"
+    val conanArch = "armv8"
+    val compileDir = layout.buildDirectory.dir("native/compile").get()
     val installDir = layout.buildDirectory.dir("native/install").get()
     doFirst {
         println(Jvm.current().javaHome.absolutePath)
@@ -88,18 +101,22 @@ val compileDarwin = tasks.create("compileDarwin") {
                 platformInstallDir.mkdirs()
             }
             exec {
-                workingDir = layout.projectDirectory.dir("src/cpp/").asFile
+                workingDir = layout.projectDirectory.asFile
                 commandLine(
                     "sh", "-c",
                     "cmake" +
                             " -B${platformCompileDir.absolutePath}" +
-                            " -DCMAKE_BUILD_TYPE=Release" +
+                            " -DCMAKE_BUILD_TYPE=${buildType.name}" +
                             " -DCMAKE_CXX_FLAGS=-std=c++17" +
-                            " -DCMAKE_DESTINATION_OS=$os" +
-                            " -DCMAKE_DESTINATION_ARCHITECTURE=\"$ARCH\"" +
                             " -DJAVA_HOME=\"${Jvm.current().javaHome.absolutePath}\"" +
-                            " -DCMAKE_INSTALL_PREFIX=\"${platformInstallDir.absolutePath}\"",
+                            " -DCMAKE_INSTALL_PREFIX=\"${platformInstallDir.absolutePath}\"" +
+                            " -DCMAKE_TOOLCHAIN_FILE=\"${layout.buildDirectory.asFile.get().absolutePath}/conan/build/macos-$conanArch/${buildType.name}/generators/conan_toolchain.cmake\""
                 )
+            }
+
+            exec {
+                workingDir = platformCompileDir
+                commandLine("sh", "-c", "cmake --build .")
             }
 
             exec {
@@ -153,3 +170,66 @@ tasks.create("zipAndroidNative") {
 }
 
 private val privmxEndpointJavaVersion get() = project(":privmx-endpoint").version
+
+object AndroidProfileConfig {
+    val Properties.sdkPath: String? get() = this["sdk.dir"] as? String
+    val Properties.ndkVersion: String? get() = this["ndk.version"] as? String
+    val Properties.ndkPath: String?
+        get() = if (sdkPath == null || ndkVersion == null) throw IllegalArgumentException(
+            "sdk.dir or ndk.version is not defined in local.properties"
+        ) else "$sdkPath/ndk/$ndkVersion"
+}
+
+tasks.register("buildAndroidWithConan") {
+    val conanArchsMap = mapOf(
+        "armeabi-v7a" to "armv7",
+        "arm64-v8a" to "armv8",
+        "x86" to "x86",
+        "x86_64" to "x86_64"
+    )
+    doFirst {
+        val ndkPath = with(AndroidProfileConfig) { localProperties.ndkPath }
+        androidArchs.forEach { arch ->
+            val conanArch = conanArchsMap[arch]
+                ?: throw IllegalArgumentException("Not supported android $arch architecture by conan")
+            exec {
+                workingDir = layout.projectDirectory.asFile
+                commandLine(
+                    "sh", "-c",
+                    "conan install ." +
+                            " -pr ./conan/profiles/android" +
+                            " -s build_type=${buildType.name}" +
+                            " -s arch=${conanArch}" +
+                            " --build missing" +
+                            " --deployer=runtime_deploy" +
+                            " --output-folder=build/conan" +
+                            " --deployer-folder build/native/install/Android/$privmxEndpointJavaVersion/$arch" +
+                            " -c \"tools.android:ndk_path=$ndkPath\"" +
+                            " -c \"tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch']\""
+                )
+            }
+        }
+    }
+}
+
+tasks.register("buildMacosWithConan") {
+    doFirst {
+        val conanArch = "armv8"
+        val arch = "arm64"
+        exec {
+            workingDir = layout.projectDirectory.asFile
+            commandLine(
+                "sh", "-c",
+                "conan install ." +
+                        " -pr ./conan/profiles/macos" +
+                        " -s build_type=${buildType.name}" +
+                        " -s arch=${conanArch}" +
+                        " --build missing" +
+                        " --deployer=runtime_deploy" +
+                        " --output-folder=build/conan" +
+                        " --deployer-folder build/native/install/Darwin/$privmxEndpointJavaVersion/$arch" +
+                        " -c \"tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch']\""
+            )
+        }
+    }
+}
