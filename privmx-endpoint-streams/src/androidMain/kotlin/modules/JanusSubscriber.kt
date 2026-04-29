e98cfa13c9c561b8029bfe9a257c1d4e7fc339bc
Doominika <dsluzynska@simplito.com>
2026-04-29 11:01:16 +0200

    feat(privmx-endpoint-streams/android): implement JanusSubscriber

---
+++ privmx-endpoint-streams/src/androidMain/kotlin/modules/JanusSubscriber.kt
@@ -0,0 +1,31 @@
+package modules
+
+import org.webrtc.MediaConstraints
+import org.webrtc.PeerConnectionFactory
+import org.webrtc.PmxKeyStore
+import org.webrtc.SessionDescription
+import java.util.concurrent.CompletableFuture
+
+class JanusSubscriber(
+    pcFactory: PeerConnectionFactory,
+    keyStore: PmxKeyStore,
+    observer: TrackObserver?,
+    onTrickle: (Long, String) -> Unit
+) : JanusConnection(pcFactory, keyStore, observer, onTrickle) {
+    fun createAnswer(offerSdp: String, type: String): String {
+        val res = CompletableFuture<SessionDescription>()
+        peerConnection.setRemoteDescription(
+            SdpObserver(null),
+            SessionDescription(SessionDescription.Type.fromCanonicalForm(type), offerSdp)
+        )
+        peerConnection.createAnswer(SdpObserver(res), MediaConstraints())
+
+        return runCatching {
+            val answer = res.get()
+            peerConnection.setLocalDescription(SdpObserver(null), answer)
+            answer.description
+        }.getOrElse {
+            throw RuntimeException("Cannot create answer")
+        }
+    }
+}
