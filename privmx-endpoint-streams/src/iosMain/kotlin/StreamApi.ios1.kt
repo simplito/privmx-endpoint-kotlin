d8439328614221c40dad061f49ac9ed12682b011
Doominika <dsluzynska@simplito.com>
2026-06-11 11:03:49 +0200

    feat(privmx-endpoint-stream): add streamApi implementation for ios

--- privmx-endpoint-streams/src/iosMain/kotlin/StreamApi.ios1.kt
+++ privmx-endpoint-streams/src/iosMain/kotlin/StreamApi.ios1.kt
@@ -1,3 +1,49 @@
-actual data class StreamApiInit(
-    val a: String
+@file:OptIn(ExperimentalForeignApi::class)
+
+import WebRTCFramework.RTCDefaultVideoDecoderFactory
+import WebRTCFramework.RTCDefaultVideoEncoderFactory
+import WebRTCFramework.RTCIceServer
+import WebRTCFramework.RTCPeerConnectionFactory
+import WebRTCFramework.RTCPeerConnectionFactoryOptions
+import kotlinx.cinterop.ExperimentalForeignApi
+
+actual class StreamApiInit
+
+private fun DefaultPeerConnectionFactory(
+    options: RTCPeerConnectionFactoryOptions
+): RTCPeerConnectionFactory = RTCPeerConnectionFactory(
+    RTCDefaultVideoEncoderFactory(),
+    RTCDefaultVideoDecoderFactory()
 )
+
+actual fun StreamApi.initialFun() {
+    this.pcManager = PeerConnectionManager(
+        DefaultPeerConnectionFactory(
+            RTCPeerConnectionFactoryOptions(),
+        ),
+        onTrickle = { sessionId, rtcConfiguration ->
+            this.api.trickle(sessionId, rtcConfiguration)
+        },
+        acceptOfferOnReconfigure = {_ , _ -> TODO("Not implemented yet.") }
+    )
+}
+
+actual fun StreamApi.joinStreamRoom(streamRoomId: String) {
+    //TODO: Rollback this change, it is do only for run test
+    val session: RoomJanusSession = pcManager.createSession(streamRoomId)
+    api.joinStreamRoom(streamRoomId, session.webrtc)
+}
+
+internal actual fun StreamApi.getRTCConfiguration(): List<IceServer> {
+    return this.api.getTurnCredentials().map { item ->
+        RTCIceServer(
+            listOf(item.url),
+            item.username,
+            item.password
+        )
+    }
+}
+
+actual fun StreamApi.dropBrokenFrames(streamRoomId: String, enable: Boolean) {
+    TODO("Not implemented - probably not available in iOS.")
+}
--- privmx-endpoint-streams/src/iosMain/kotlin/TrackObserver.kt
+++
@@ -1,7 +0,0 @@
-import WebRTCFramework.RTCMediaStreamTrack
-import kotlinx.cinterop.ExperimentalForeignApi
-
-@OptIn(ExperimentalForeignApi::class)
-fun interface TrackObserver{
-    fun onRemoteTrack(streamId: String?, track: RTCMediaStreamTrack)
-}
