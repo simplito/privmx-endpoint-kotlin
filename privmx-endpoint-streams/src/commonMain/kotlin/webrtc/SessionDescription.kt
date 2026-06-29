475d93f7d7243428c52b103d97173895ba2ea347
Doominika <dsluzynska@simplito.com>
2026-06-29 12:03:13 +0200

    chore(privmx-endpoint-streams): change package name

--- privmx-endpoint-streams/src/commonMain/kotlin/webrtc/SessionDescription.kt
+++
@@ -1,6 +0,0 @@
-package webrtc
-
-expect class SessionDescription
-
-internal expect val SessionDescription.sdp: String
-internal expect fun sessionDescription(type: String, sdp: String): SessionDescription
