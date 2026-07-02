9ff3ca3fca8b4649a134a750630ca26c400cccb9
Doominika <dsluzynska@simplito.com>
2026-07-02 13:45:13 +0200

    refactor: remove StreamEventData class

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/StreamEventData.kt
+++
@@ -1,7 +0,0 @@
-package com.simplito.kotlin.privmx_endpoint.model.stream.events
-
-data class StreamEventData(
-    val streamRoomId: String,
-    val streamIds: List<Long>,
-    val userId: String
-)
