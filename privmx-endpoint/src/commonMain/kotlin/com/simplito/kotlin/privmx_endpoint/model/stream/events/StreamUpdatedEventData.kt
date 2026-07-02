0f08018b6c87aeb5577d2b636ce96f444ef6b84f
Doominika <dsluzynska@simplito.com>
2026-07-02 13:24:21 +0200

    refactor: update StreamUpdatedEventData class

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/StreamUpdatedEventData.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/StreamUpdatedEventData.kt
@@ -1,11 +1,13 @@
 package com.simplito.kotlin.privmx_endpoint.model.stream.events
 
-import com.simplito.kotlin.privmx_endpoint.model.stream.StreamInfo
-import com.simplito.kotlin.privmx_endpoint.model.stream.StreamTrackModification
+import com.simplito.kotlin.privmx_endpoint.model.stream.StreamTrackInfo
+import com.simplito.kotlin.privmx_endpoint.model.stream.StreamTrackModificationPair
 
 data class StreamUpdatedEventData(
     val streamRoomId: String,
-    val streamsAdded: List<StreamInfo>,
-    val streamsRemoved: List<StreamInfo>,
-    val streamsModified: List<StreamTrackModification>,
+    val streamId: Long,
+    val userId: String,
+    val tracksAdded: List<StreamTrackInfo>,
+    val tracksRemoved: List<StreamTrackInfo>,
+    val tracksModified: List<StreamTrackModificationPair>,
 )
