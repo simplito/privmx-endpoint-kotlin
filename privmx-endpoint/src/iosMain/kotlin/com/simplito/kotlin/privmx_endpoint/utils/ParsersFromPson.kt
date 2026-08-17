3a6183f037811bed87d9cf4df2b4b2f40e41ae55
Doominika <dsluzynska@simplito.com>
2026-08-17 13:43:26 +0200

    fix: remove duplicate timestamp mapping in toEvent

--- privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/utils/ParsersFromPson.kt
+++ privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/utils/ParsersFromPson.kt
@@ -277,7 +277,6 @@ internal fun PsonObject.toEvent(): Event<*> = Event(
     this["channel"]!!.typedValue(),
     this["connectionId"]?.typedValue(),
     this["subscriptions"]!!.typedList().map { it.typedValue() },
-    this["timestamp"]!!.typedValue(),
     this["timestamp"]?.typedValue(),
     (this["data"] as PsonObject?)?.let {
         EventDataMappers[it.type]?.invoke(it)
