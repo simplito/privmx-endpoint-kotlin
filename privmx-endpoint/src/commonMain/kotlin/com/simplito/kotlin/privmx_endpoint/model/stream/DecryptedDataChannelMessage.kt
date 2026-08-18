e4613672766700d25436e09ce120a9004437dee6
Doominika <dsluzynska@simplito.com>
2026-08-18 15:53:40 +0200

    docs: add docs to DecryptedDataChannelMessage

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/DecryptedDataChannelMessage.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/DecryptedDataChannelMessage.kt
@@ -1,7 +1,16 @@
 package com.simplito.kotlin.privmx_endpoint.model.stream
 
+/**
+ * Holds a decrypted message received over a Stream's data channel.
+ *
+ * @property statusCode     status code of decryption of the message.
+ *                          A value other than 0 means that the message could not be decrypted or
+ *                          that its data integrity has been violated
+ * @param data              message's plain (unencrypted) content
+ * @param seq               message's sequence number, used to detect replayed and out-of-order messages
+ */
 class DecryptedDataChannelMessage(
     val statusCode: Long,
     data: ByteArray,
     seq: Long
-): DataChannelMessage(data, seq)
+) : DataChannelMessage(data, seq)
