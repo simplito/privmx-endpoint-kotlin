085d0fcaa99ae8d837d1a8605d36bad2b9e62445
Doominika <dsluzynska@simplito.com>
2026-08-18 15:55:06 +0200

    docs: add docs to KeyType

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/KeyType.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/KeyType.kt
@@ -1,6 +1,16 @@
 package com.simplito.kotlin.privmx_endpoint.model.stream
 
+/**
+ * Types of keys used to encrypt and decrypt Stream data.
+ */
 enum class KeyType {
+    /**
+     * key used to encrypt the data sent by the local user
+     */
     LOCAL,
+
+    /**
+     * key used to decrypt the data received from other participants
+     */
     REMOTE
 }
