fe56e296eb181c809a64c9af7c6c5dcbef306c0d
Doominika <dsluzynska@simplito.com>
2026-08-18 15:53:13 +0200

    docs: add docs to DataChannelMessage

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/DataChannelMessage.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/DataChannelMessage.kt
@@ -1,5 +1,11 @@
 package com.simplito.kotlin.privmx_endpoint.model.stream
 
+/**
+ * Holds a message sent over a Stream's data channel.
+ *
+ * @property data   message's plain (unencrypted) content
+ * @property seq    message's sequence number, used to detect replayed and out-of-order messages
+ */
 open class DataChannelMessage(
     val data: ByteArray,
     val seq: Long
