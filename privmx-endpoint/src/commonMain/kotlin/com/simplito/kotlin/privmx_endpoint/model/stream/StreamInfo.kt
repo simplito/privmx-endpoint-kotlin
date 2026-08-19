da6f03a0cd1965e3c73c7919b792d71f46f8351d
Doominika <dsluzynska@simplito.com>
2026-08-19 11:54:27 +0200

    docs: update docs for StreamInfo

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/StreamInfo.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/StreamInfo.kt
@@ -3,11 +3,11 @@ package com.simplito.kotlin.privmx_endpoint.model.stream
 /**
  * Contains detailed information about a stream and its associated tracks.
  *
- * @property id       Unique identifier of the stream
- * @property userId   Identifier of the user who published the stream
- * @property metadata Additional metadata attached to the stream as a JSON string
- * @property dummy    //todo
- * @property tracks   Information about the tracks within the stream
+ * @property id         ID of the Stream
+ * @property userId     ID of the user who published the stream
+ * @property metadata   additional metadata attached to the stream as a JSON string
+ * @property dummy      determines whether the Stream is a dummy (placeholder) Stream
+ * @property tracks     information about the tracks within the stream
  */
 data class StreamInfo(
     val id: Long?,
