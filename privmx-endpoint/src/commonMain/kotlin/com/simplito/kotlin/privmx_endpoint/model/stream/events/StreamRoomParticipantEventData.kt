01c907213104e7be2b7e517c187fe0de3c5def81
Doominika <dsluzynska@simplito.com>
2026-08-19 11:56:17 +0200

    docs: update docs for StreamRoomParticipantEventData

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/StreamRoomParticipantEventData.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/events/StreamRoomParticipantEventData.kt
@@ -1,10 +1,12 @@
 package com.simplito.kotlin.privmx_endpoint.model.stream.events
 
 /**
- * @property streamRoomId StreamRoom ID
- * @property userId User ID of the member who joined or left
+ * Data describing the event triggered when a participant joins or leaves a Stream Room.
+ *
+ * @property streamRoomId   StreamRoom ID
+ * @property userId         User ID of the member who joined or left
  */
-data class StreamRoomParticipantEventData (
+data class StreamRoomParticipantEventData(
     val streamRoomId: String,
     val userId: String
 )
