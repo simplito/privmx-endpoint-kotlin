1f420301bc389adffd0648b303dab4058979886d
Doominika <dsluzynska@simplito.com>
2026-08-19 11:57:37 +0200

    docs: update docs for StreamTrackModificationPair

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/StreamTrackModificationPair.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/StreamTrackModificationPair.kt
@@ -3,8 +3,8 @@ package com.simplito.kotlin.privmx_endpoint.model.stream
 /**
  * Represents a single track modification, holding the state of a track before and after the change.
  *
- * @property before The state of the track before the modification
- * @property after  The state of the track after the modification
+ * @property before state of the track before the modification, or no value if the track has just been added
+ * @property after  state of the track after the modification, or no value if the track has been removed
  */
 data class StreamTrackModificationPair(
     val before: StreamTrackInfo?,
