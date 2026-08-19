0ede7711f56a96afe14ef575149c84a28fa91099
Doominika <dsluzynska@simplito.com>
2026-08-19 11:54:40 +0200

    docs: update docs for StreamPublishResult

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/StreamPublishResult.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/stream/StreamPublishResult.kt
@@ -3,9 +3,9 @@ package com.simplito.kotlin.privmx_endpoint.model.stream
 /**
  * Represents the result of a stream publish/update operation.
  *
- * @property published Whether the stream was successfully published
- * @property data Additional information about the published stream
-*/
+ * @property published  whether the stream was successfully published
+ * @property data       additional information about the published stream
+ */
 data class StreamPublishResult(
     val published: Boolean,
     val data: PublishedStreamData?,
