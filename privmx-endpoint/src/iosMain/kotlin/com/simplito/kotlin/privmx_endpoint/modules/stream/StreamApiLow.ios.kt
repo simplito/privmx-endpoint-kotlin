e94820c43688521014f705d8f4c4e1e13bbe7a26
Doominika <dsluzynska@simplito.com>
2026-09-01 11:41:35 +0200

    docs: update StreamApiLow.createStreamRoom description

--- privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/stream/StreamApiLow.ios.kt
+++ privmx-endpoint/src/iosMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/stream/StreamApiLow.ios.kt
@@ -113,6 +113,9 @@ actual constructor(
      * @param publicMeta public metadata
      * @param privateMeta private metadata
      * @param policies additional container access policies
+     * @param emptyRoomTtl grace period (ms) the Stream Room stays open after the last participant leaves;
+     * 0 closes it immediately; null use the server default (closes it immediately)
+     *
      * @return ID of the created room
      * @throws PrivmxException thrown when method encounters an exception
      * @throws NativeException thrown when method encounters an unknown exception
