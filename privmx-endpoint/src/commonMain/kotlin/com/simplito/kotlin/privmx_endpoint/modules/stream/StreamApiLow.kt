000bd44394ab5e8e39d808de65be524f27594397
Doominika <dsluzynska@simplito.com>
2026-09-01 11:51:10 +0200

    docs: update StreamApiLow.removeStream description

--- privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/stream/StreamApiLow.kt
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/modules/stream/StreamApiLow.kt
@@ -280,9 +280,13 @@ constructor(
     fun updateStream(streamHandle: StreamHandle): StreamPublishResult
 
     /**
-     * Unpublishes a stream.
+     * Stops publishing and closes the Publisher Stream.
+     *
+     * The handle is closed after this call and cannot be used anymore, but a new Publisher Stream can be created
+     * in the same Stream Room with [createStream].
      *
      * @param streamHandle handle to the stream to unpublish
+     *
      * @throws PrivmxException thrown when method encounters an exception
      * @throws NativeException thrown when method encounters an unknown exception
      * @throws IllegalStateException thrown when instance is closed
