483d4ea2a917c2961bc7160bf23a59417fd95c74
Doominika <dsluzynska@simplito.com>
2026-09-07 15:29:01 +0200

    refactor: require sources when creating tracks

--- privmx-endpoint-streams/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_streams/webrtc/PeerConnectionFactory.kt
+++ privmx-endpoint-streams/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint_streams/webrtc/PeerConnectionFactory.kt
@@ -4,14 +4,25 @@ expect class PeerConnectionFactory
 
 internal expect fun PeerConnectionFactory.createPeerConnection(observer: Observer): PeerConnection
 internal expect fun PeerConnectionFactory.disposeFactory()
-internal expect fun PeerConnectionFactory.makeVideoTrack(
-    id: String,
-    isScreenCast: Boolean,
-    alignTimestamps: Boolean = true
-): VideoTrack
-
-internal expect fun PeerConnectionFactory.makeAudioTrack(id: String): AudioTrack
 internal expect fun PeerConnectionFactory.createSenderFrameCryptor(
     sender: RtpSender,
     keyStore: KeyStore
 ): FrameCryptor
+
+internal expect fun PeerConnectionFactory.makeAudioSource(): AudioSource
+internal expect fun PeerConnectionFactory.makeAudioTrack(
+    id: String,
+    audioSource: AudioSource?
+): AudioTrack
+
+internal expect fun PeerConnectionFactory.makeVideoSource(
+    isScreenCast: Boolean = false,
+    alignTimestamps: Boolean = true
+): VideoSource
+
+internal expect fun PeerConnectionFactory.makeVideoTrack(
+    id: String,
+    source: VideoSource,
+): VideoTrack
+
+
