8a3ab8a93f71ce9dba864bfb2ad3856806d40759
Doominika <dsluzynska@simplito.com>
2026-09-10 11:53:55 +0200

    feat: add models parsers

--- jni-wrapper/src/cpp/model_native_initializers.h
+++ jni-wrapper/src/cpp/model_native_initializers.h
@@ -199,6 +199,20 @@ namespace privmx {
         jobject groupCustomEventData2Java(JniContextUtils &ctx,
                 const privmx::endpoint::group::GroupCustomEventData &data);
 
+        jobject group2Java(JniContextUtils &ctx, privmx::endpoint::group::Group group_c);
+
+        jobject groupSummary2Java(JniContextUtils &ctx,
+                privmx::endpoint::group::GroupSummary groupSummary_c);
+
+        jobject envelopeType2Java(JniContextUtils &ctx,
+                privmx::endpoint::group::EnvelopeType envelopeType_c);
+
+        jobject decryptedEnvelope2Java(JniContextUtils &ctx,
+                privmx::endpoint::group::DecryptedEnvelope decryptedEnvelope_c);
+
+        jobject decryptedFileInfo2Java(JniContextUtils &ctx,
+                privmx::endpoint::group::DecryptedFileInfo decryptedFileInfo_c);
+
         //Streams
         jobject streamRoom2Java(
                 JniContextUtils &ctx,
