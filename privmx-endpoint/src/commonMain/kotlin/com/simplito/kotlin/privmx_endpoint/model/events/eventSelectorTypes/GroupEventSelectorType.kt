e1bc343e9026a08c3977badd453f3dd6846d438b
Doominika <dsluzynska@simplito.com>
2026-09-10 10:26:13 +0200

    feat: add models required for GroupApi in common source

---
+++ privmx-endpoint/src/commonMain/kotlin/com/simplito/kotlin/privmx_endpoint/model/events/eventSelectorTypes/GroupEventSelectorType.kt
@@ -0,0 +1,29 @@
+/*
+ *
+ * PrivMX Endpoint Kotlin.
+ * Copyright © 2025 Simplito sp. z o.o.
+ *
+ * This file is part of the PrivMX Platform (https://privmx.dev).
+ * This software is Licensed under the MIT License.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.simplito.kotlin.privmx_endpoint.model.events.eventSelectorTypes
+
+/**
+ * Specifies the type of identifier used to select a Group event.
+ */
+enum class GroupEventSelectorType : EventSelectorType {
+    /**
+     * Selects events based on the ID of the context.
+     */
+    CONTEXT_ID,
+
+    /**
+     * Selects events based on the ID of the Group.
+     */
+    GROUP_ID
+}
