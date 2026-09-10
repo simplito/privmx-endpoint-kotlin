ee65d8a12e62cc471a36856316b208e826fbf20e
Dawid Jenczewski <djenczewski@simplito.com>
2026-09-10 16:52:40 +0200

    chore: Add build from sources for Linux compilation

--- jni-wrapper/build.gradle.kts
+++ jni-wrapper/build.gradle.kts
@@ -198,52 +198,101 @@ val compileDarwin = tasks.create("compileDarwin") {
 }
 
 //TODO: Use cpp-library plugin compilation tasks
-val compileLinux = tasks.create("compileLinux") {
+val compileLinux = tasks.register("compileLinux") {
     group = "privmx native"
-    dependsOn("buildLinuxWithConan")
     val os = "Linux"
     val conanArch = "x86_64"
     val arch = "x86_64"
     val compileDir = layout.buildDirectory.dir("native/compile").get()
     val installDir = layout.buildDirectory.dir("native/install").get()
+    var usePrebuiltEndpoint: Boolean = false
+    if(buildType == BuildTypes.MinSizeRel || buildType == BuildTypes.Release) {
+        dependsOn("buildLinuxWithConan")
+    }else{
+        dependsOn("buildLinuxFromSources")
+        usePrebuiltEndpoint = true
+    }
+    val prebuildEndpointDir = layout.buildDirectory.dir("endpoint-prebuild/install").get()
     doFirst {
-        val platformInstallDir = installDir.file("$os/$privmxEndpointJavaVersion/$arch").asFile
-        val platformCompileDir = compileDir.file("$os/$privmxEndpointJavaVersion/$arch").asFile
-        if (!platformCompileDir.exists()) {
-            platformCompileDir.mkdirs()
-        }
-        if (!platformInstallDir.exists()) {
-            platformInstallDir.mkdirs()
-        }
-        exec {
-            workingDir = layout.projectDirectory.asFile
-            commandLine(
-                "sh", "-c",
-                "cmake" +
-                        " -B${platformCompileDir.absolutePath}" +
-                        " -DCMAKE_BUILD_TYPE=${buildType.name}" +
-                        " -DCMAKE_CXX_FLAGS=-std=c++17" +
-                        " -DJAVA_HOME=\"${Jvm.current().javaHome.absolutePath}\"" +
-                        " -DCMAKE_INSTALL_PREFIX=\"${platformInstallDir.absolutePath}\"" +
-                        " -DCMAKE_TOOLCHAIN_FILE=\"${layout.buildDirectory.asFile.get().absolutePath}/conan/build/linux-$conanArch/${buildType.name}/generators/conan_toolchain.cmake\""
-            )
-        }
+        println(Jvm.current().javaHome.absolutePath)
+        darwinArchs.onEach { ARCH ->
+            val platformInstallDir = installDir.file("$os/$privmxEndpointJavaVersion/$arch").asFile
+            val platformCompileDir = compileDir.file("$os/$privmxEndpointJavaVersion/$arch").asFile
+            if (!platformCompileDir.exists()) {
+                platformCompileDir.mkdirs()
+            }
+            if (!platformInstallDir.exists()) {
+                platformInstallDir.mkdirs()
+            }
+            val endpointArgs = if (usePrebuiltEndpoint) {
+                " -DPRIVMX_USE_PREBUILT=ON" +
+                        " -DPRIVMX_PREBUILT_DIR=\"${prebuildEndpointDir.file("$os/$privmxEndpointJavaVersion/$ARCH").asFile.absolutePath}\""
+            } else {
+                " -DCMAKE_TOOLCHAIN_FILE=\"${layout.buildDirectory.asFile.get().absolutePath}/conan/build/linux-$conanArch/${buildType.name}/generators/conan_toolchain.cmake\""
+            }
+            exec {
+                workingDir = layout.projectDirectory.asFile
+                commandLine(
+                    "sh", "-c",
+                    "cmake" +
+                            " -B${platformCompileDir.absolutePath}" +
+                            " -DCMAKE_BUILD_TYPE=${buildType.name}" +
+                            " -DCMAKE_CXX_FLAGS=-std=c++17" +
+                            " -DJAVA_HOME=\"${Jvm.current().javaHome.absolutePath}\"" +
+                            " -DCMAKE_INSTALL_PREFIX=\"${platformInstallDir.absolutePath}\"" +
+                            endpointArgs
+                )
+            }
+            exec {
+                workingDir = platformCompileDir
+                commandLine("sh", "-c", "cmake --build .")
+            }
 
-        exec {
-            workingDir = platformCompileDir
-            commandLine("sh", "-c", "cmake --build .")
-        }
+            exec {
+                workingDir = platformCompileDir
+                commandLine("sh", "-c", "make -s -j8")
+            }
+            exec {
+                workingDir = platformCompileDir
+                commandLine("sh", "-c", "make -s install")
+            }
 
-        exec {
-            workingDir = platformCompileDir
-            commandLine("sh", "-c", "make -s -j8")
+            fileTree(platformInstallDir) { include("**/*.so") }.forEach { soFile ->
+                exec {
+                    commandLine("patchelf", "--set-rpath", "\$ORIGIN", soFile.absolutePath)
+                }
+            }
         }
-        exec {
-            workingDir = platformCompileDir
-            commandLine("sh", "-c", "make -s install")
-        }
+    }
+}
 
-        fileTree(platformInstallDir) { include("**/*.so") }.forEach { soFile ->
+val compileLinux = tasks.create("buildLinuxFromSources") {
+    group = "privmx native"
+    dependsOn("clonePrivmxSources")
+//    onlyIf { !layout.buildDirectory.get().dir("endpoint-prebuild/install/Darwin/$privmxEndpointJavaVersion/arm64").asFile.exists() }
+    doFirst {
+        val conanArch = "x86_64"
+        val arch = "x86_64"
+        val clonedEndpointDir = layout.buildDirectory.dir("privmx-endpoint").get().asFile
+        val profile = layout.projectDirectory.file("conan/profiles/linux-x86_64").asFile
+        val INSTALL_DIR =
+            layout.buildDirectory.get().dir("endpoint-prebuild/install/Linux/$privmxEndpointJavaVersion/$arch").asFile
+        val COMPILED_DIR =
+            layout.buildDirectory.get().dir("endpoint-prebuild/compile/Linux/$privmxEndpointJavaVersion/$arch").asFile
+        conanInstall(
+            clonedEndpointDir,
+            profile,
+            "../conan",
+            "../endpoint-prebuild/install/Linux/$privmxEndpointJavaVersion/$arch",
+            additionalParams = listOf(" -s arch=${conanArch}"),
+        )
+        buildFromSources(
+            INSTALL_DIR,
+            COMPILED_DIR,
+            "${layout.buildDirectory.asFile.get().absolutePath}/conan/build/linux-$conanArch/${buildType.name}/generators/conan_toolchain.cmake",
+        )
+        copyFilesFromDeploy(INSTALL_DIR,".so")
+        fileTree(INSTALL_DIR) { include("**/*.so") }.forEach { soFile ->
             exec {
                 commandLine("patchelf", "--set-rpath", "\$ORIGIN", soFile.absolutePath)
             }
@@ -725,9 +774,9 @@ private fun Project.copyFilesFromDeploy(
         } else {
             include(
                 "**/libPoco*$extension",
-                "**/libcrypto*$extension",
+                "**/libcrypto*$extension*",
                 "**/libPson*$extension",
-                "**/libssl*$extension",
+                "**/libssl*$extension*",
                 "**/libprivmx*$extension"
             )
         }
